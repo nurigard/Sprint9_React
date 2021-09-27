804efc9795a15ae0a96cffc232c81bf06006e5f2
Nuria Gardenyes <gardenyesnuria@gmail.com>
2021-09-27 18:54:14 +0200

    component VideoItem dubte Error

--- src/components/videolist/VideoList.js
+++ src/components/videolist/VideoList.js
@@ -2,16 +2,17 @@ import VideoItem from "./videoitem/VideoItem"
 
 const VideoList = ({llistaVideos}) =>{
 
-    //<div>{llistaVideos}</div>
- 
     return (
-        <div>
-         
-        {(llistaVideos) && (llistaVideos.map((videoUnitat, index) => <VideoItem key={index} videoLLista={videoUnitat} />))}
+    <div>
 
-        </div>
+    {(llistaVideos) && (llistaVideos.map(({videoUnitat, index}) => <VideoItem key={index} videoLLista={videoUnitat} />))}  
+     
+    </div>
     )
 
 }
 
  export default VideoList;
+
+ //Per a que no em surti l'error "objects are not valid as a react childs ..." he de posar els valors entre {}. A la linia 10 he posat (videoUnitat, index) entre {} 
+ 
--- src/components/videolist/videoitem/VideoItem.js
+++ src/components/videolist/videoitem/VideoItem.js
@@ -1,5 +1,17 @@
 
-const VideoItem = (props) => <div>{props.videoLLista}</div>
+const VideoItem = ({videoLLista}) => {
 
+    console.log('videoLLista', videoLLista);
 
+    const imatgeVideo = videoLLista.id.videoId;
+    const titolVideo = videoLLista.snippet.title;
+
+    return(
+        <div>
+            {imatgeVideo}
+            <h2>{titolVideo}</h2>
+        </div>
+
+    )
+}
 export default VideoItem;
